b430870f8c37c3d37f1be82ed88f719b9c058adb
Sean M Paus <spaus@nextgen.com>
2018-07-12 16:54:16 -0500

    Initial Commit

---
+++ TestDictation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestDictation
+//
+//  Created by Sean Paus on 7/12/18.
+//  Copyright © 2018 Spaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
